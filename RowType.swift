921cc22362821ff619555dd0de8c1ee48f576f5d
Rashwan Lazkani <rashwan87@gmail.com>
2015-06-23 17:15:17 +0200

    La till en ny enum för att hantera rader i widget

---
+++ RowType.swift
@@ -0,0 +1,20 @@
+//
+//  Typ.swift
+//  Tajma
+//
+//  Created by Rashwan Lazkani on 2015-06-22.
+//  Copyright (c) 2015 Rashwan Lazkani. All rights reserved.
+//
+
+import Foundation
+
+public enum RowType {
+    case BlankRow
+    case Button
+    case ButtonAddStop
+    case Info
+    case NoDepartures
+    case Stop
+    case Trip
+    case TurnOnLocation
+}
