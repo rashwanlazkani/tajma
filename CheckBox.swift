041cfe2d83849ff3c077246a87c2d279b4e17944
Rashwan Lazkani <rashwan87@gmail.com>
2015-10-28 17:27:53 +0100

    Kod refaktorering

--- CheckBox.swift
+++ CheckBox.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class CheckBox: UIButton {
-    // images
     let checkedImage = UIImage(named: "check-box-red") as UIImage!
     let uncheckedImage = UIImage(named: "unchecked-box") as UIImage!
     
-    // bool properties
     var isChecked : Bool = false{
-        // varje gång isChecked ändras anropas didSet
         didSet{
             if (isChecked == true){
                 self.setImage(checkedImage, forState: .Normal)
                 self.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
-                
                 self.sendSubviewToBack(self)
             }
             else{
@@ -32,33 +28,15 @@ class CheckBox: UIButton {
     
     override func awakeFromNib(){
         self.addTarget(self, action: "buttonClicked:", forControlEvents: UIControlEvents.TouchUpInside)
-        self.isChecked = false
     }
     
     func buttonClicked(sender : UIButton){
-        
-        
-        //var linesView: LinesViewController()
-        
-        if (isChecked == true){
-            for stopline in Global.linesAtStop{
-                if (stopline.tag == sender.tag){
-                    stopline.isChecked = false
-                    isChecked = false
-                    RealmService.sharedInstance.addLinesToStop(stopline)
-                    
-                    return
-                }
-            }
-        }
-        else if (isChecked == false){
-            for stopline in Global.linesAtStop{
-                if (stopline.tag == sender.tag){
-                    stopline.isChecked = true
-                    isChecked = true
-                    RealmService.sharedInstance.addLinesToStop(stopline)
-                    return
-                }
+        isChecked = !isChecked
+        for stopline in Global.linesAtStop{
+            if (stopline.tag == sender.tag){
+                stopline.isChecked = isChecked
+                RealmService.sharedInstance.updateLinesToStop(stopline)
+                return
             }
         }
     }
