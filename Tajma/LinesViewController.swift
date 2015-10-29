a726d24d8ec0abe5cd08aa8fa6f2d7ca3bfe7f0c
Rashwan Lazkani <rashwan87@gmail.com>
2015-10-29 23:06:34 +0100

    Refekatorering av kod

--- Tajma/LinesViewController.swift
+++ Tajma/LinesViewController.swift
@@ -17,22 +17,12 @@ class LinesViewController: UIViewController {
     var stop : Stop!
     var currentLinesAndDirections = [String]()
     let phoneSize = PhoneSize()
-    var checkBoxService = CheckBox()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationController?.navigationBar.hidden = false
-
-        self.scrollView.bounces = true
-        self.scrollView.alwaysBounceVertical = true
-        
         initiateViews()
         drawLinesTableView()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        self.navigationController?.navigationBar.layer.zPosition = 1
     }
     
     override func willMoveToParentViewController(parent: UIViewController?) {
@@ -45,8 +35,10 @@ class LinesViewController: UIViewController {
     
     // MARK: - Functions
     func initiateViews(){
-        // NavController
-        navigationController?.navigationBar.hidden = false
+        self.scrollView.bounces = true
+        self.scrollView.alwaysBounceVertical = true
+        
+        self.navigationController?.navigationBar.hidden = false
         self.navigationController?.navigationBar.barStyle = UIBarStyle.Black
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 231/255, green: 63/255, blue: 87/255, alpha: 1)
@@ -56,13 +48,12 @@ class LinesViewController: UIViewController {
         title.textAlignment = NSTextAlignment.Center
         title.textColor = UIColor.whiteColor()
         title.font = title.font.fontWithSize(19)
-        title.text = stop.name
+        title.text = stop.name.componentsSeparatedByString(",").first
         
-        let navBarTitleView = UIView(frame: CGRect(x: phoneSize.width / 2, y: 0, width: 200, height: 44))
-        navBarTitleView.backgroundColor = UIColor.clearColor()
-        self.navigationItem.titleView = navBarTitleView
-        
-        navBarTitleView.addSubview(title)
+        let titleView = UIView(frame: CGRect(x: phoneSize.width / 2, y: 0, width: 200, height: 44))
+        titleView.backgroundColor = UIColor.clearColor()
+        self.navigationItem.titleView = titleView
+        titleView.addSubview(title)
         
         scrollView.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1)
     }
@@ -70,10 +61,7 @@ class LinesViewController: UIViewController {
     func drawLinesTableView(){
         var height = 0
         var tag = 0
-        
-        //self.lineWrapper.lines.sortInPlace({Int($0.sname) < Int($1.sname) ? $0.sname < $1.name : $0.name < $1.name })
         self.lineWrapper.lines.sortInPlace({Int($0.sname) < Int($1.sname)})
-        
         
         for (index, line) in lineWrapper.lines.enumerate(){
             let checkBox = CheckBox()
@@ -83,65 +71,33 @@ class LinesViewController: UIViewController {
             checkBox.frame = CGRectMake(50, 50, 1000, 44)
             checkBox.center = CGPoint(x: scrollView.bounds.width - 25, y: 44 / 2.0)
             
-            // init!
-            //var stopLine : StopLine
             let stopLine = StopLine()
-            
-            var isChecked = false
+            stopLine.stopId = stop.id
+            stopLine.stopName = stop.name
+            stopLine.lat = stop.lat
+            stopLine.long = stop.long
+            stopLine.sname = line.sname
+            stopLine.tag = checkBox.tag
+            stopLine.type = line.type
+            stopLine.track = line.track
+            stopLine.direction = line.direction
+            stopLine.lineAndDirection = line.lineAndDirection
             
             if (Global.allaStopp.contains(line.lineAndDirection)){
-                //init!
-                //stopLine = StopLine(stopId: stop.id, stopName: stop.name, lat: stop.lat, long: stop.long, sname: line.sname, tag: checkBox.tag, type: line.type, track: line.track, direction: line.direction, lineAndDirection: line.lineAndDirection, isChecked: true)
-                
-                stopLine.stopId = stop.id
-                stopLine.stopName = stop.name
-                stopLine.lat = stop.lat
-                stopLine.long = stop.long
-                stopLine.sname = line.sname
-                stopLine.tag = checkBox.tag
-                stopLine.type = line.type
-                stopLine.track = line.track
-                stopLine.direction = line.direction
-                stopLine.lineAndDirection = line.lineAndDirection
                 stopLine.isChecked = true
-                
                 checkBox.isChecked = true
-                
-                isChecked = true
             }
             else{
-                // init!
-                //stopLine = StopLine(stopId: stop.id, stopName: stop.name, lat: stop.lat, long: stop.long, sname: line.sname, tag: checkBox.tag, type: line.type, track: line.track, direction: line.direction, lineAndDirection: line.lineAndDirection, isChecked: false)
-                
-                stopLine.stopId = stop.id
-                stopLine.stopName = stop.name
-                stopLine.lat = stop.lat
-                stopLine.long = stop.long
-                stopLine.sname = line.sname
-                stopLine.tag = checkBox.tag
-                stopLine.type = line.type
-                stopLine.track = line.track
-                stopLine.direction = line.direction
-                stopLine.lineAndDirection = line.lineAndDirection
                 stopLine.isChecked = false
-                
-                
-                isChecked = false
             }
             
             Global.linesAtStop.append(stopLine)
             
             let view = UIView(frame: CGRect(x: 0, y: height, width: Int(scrollView.frame.size.width), height: 44))
-            
             var fontSize = CGFloat(16)
+            
             var sname = ""
-            let letterSname = Int(line.sname)
-            // Bokstväver
-            if (line.sname == "16X"){
-                fontSize = CGFloat(12)
-                sname = line.sname
-            }
-            else if (letterSname == nil){
+            if ((Int(line.sname.substringToIndex(line.sname.startIndex.advancedBy(1)))) == nil){
                 let snameArr = Array(line.sname.characters)
                 sname = String(snameArr[0])
             }
@@ -153,40 +109,31 @@ class LinesViewController: UIViewController {
                 sname = line.sname
             }
             
-            // SnameView
             let snameView = UIView()
             snameView.frame = CGRectMake(30, 30, 30, 30)
             snameView.layer.cornerRadius = 5
             snameView.center = CGPoint(x: 25, y: 23)
             snameView.backgroundColor = UIColor(rgba: line.fgColor)
             
-            // SnameLabel
             let snameLabel = UILabel(frame: CGRectMake(0, 0, 30, 30))
             snameLabel.textAlignment = NSTextAlignment.Center
             snameLabel.text = sname ?? line.sname
             snameLabel.textColor = UIColor(rgba: line.bgColor)
             snameLabel.font = snameLabel.font.fontWithSize(fontSize)
             
-            // DirectionLabel
             let directionLabel = UILabel(frame: CGRectMake(0, 8, DeviceHelper.getLabelWidth(), 30))
             directionLabel.textAlignment = NSTextAlignment.Left
             directionLabel.textColor = UIColor(red: 51/255, green: 51/255, blue: 51/255, alpha: 1)
             directionLabel.text = "\t     \(line.direction)"
             directionLabel.font = directionLabel.font.fontWithSize(16)
             
-            // SepartorView
             let separatorView = UIView(frame: CGRect(x: 0, y: height, width: Int(scrollView.frame.size.width), height: 1))
             separatorView.backgroundColor = UIColor(red: 204/255, green: 204/255, blue: 204/255, alpha: 0.5)
             
-            if (isChecked){
-                //directionLabel.font = UIFont.boldSystemFontOfSize(16)
-            }
-            
             view.addSubview(checkBox)
             view.addSubview(snameView)
-            snameView.addSubview(snameLabel)
             view.addSubview(directionLabel)
-            
+            snameView.addSubview(snameLabel)
             scrollView.addSubview(view)
             scrollView.addSubview(separatorView)
             
@@ -199,8 +146,7 @@ class LinesViewController: UIViewController {
                 view.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1)
             }
             
-            
-            // Sätta bakgrunden på tableView
+            // Sätter bakgrunden på tableView för att dölja tomma linjer
             if (index == lineWrapper.lines.count - 1){
                 if (index % 2 == 0){
                     scrollView.backgroundColor = UIColor(red: 246/255, green: 246/255, blue: 246/255, alpha: 1)
@@ -209,18 +155,15 @@ class LinesViewController: UIViewController {
                     scrollView.backgroundColor = UIColor(red: 249/255, green: 249/255, blue: 249/255, alpha: 1)
                 }
             }
-            
         }
-            
         
         let separatorView = UIView(frame: CGRect(x: 0, y: height, width: Int(scrollView.frame.size.width), height: 1))
         separatorView.backgroundColor = UIColor(red: 219/255, green: 219/255, blue: 219/255, alpha: 1)
         
         self.view.addSubview(separatorView)
         scrollView.contentSize = CGSize(width: phoneSize.width, height: height)
-        
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
