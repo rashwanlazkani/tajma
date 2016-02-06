58abc23672247433deb49a42a9ac64d95243cb1b
Rashwan Lazkani <rashwan87@gmail.com>
2016-02-06 10:13:49 +0100

    Lagt till titel för vyn

--- Tajma/WebViewController.swift
+++ Tajma/WebViewController.swift
@@ -11,8 +11,21 @@ import UIKit
 class WebViewController: UIViewController, UIWebViewDelegate {
     @IBOutlet weak var webView: UIWebView!
     var url = ""
+    let deviceHelper = DeviceHelper()
+    var titleForView = ""
     
     override func viewDidLoad() {
+        let titleLabel = UILabel(frame: CGRectMake(0, 7, 200, 30))
+        titleLabel.textAlignment = NSTextAlignment.Center
+        titleLabel.textColor = UIColor.whiteColor()
+        titleLabel.font = titleLabel.font.fontWithSize(19)
+        titleLabel.text = titleForView
+        
+        let titleView = UIView(frame: CGRect(x: deviceHelper.screenWidth / 2, y: 0, width: 200, height: 44))
+        titleView.backgroundColor = UIColor.clearColor()
+        self.navigationItem.titleView = titleView
+        titleView.addSubview(titleLabel)
+        
         webView.delegate = self
         webView.scalesPageToFit = true
         
