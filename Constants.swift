50777182f0961c5052ec1665ebbd2ec63e0205d4
Rashwan Lazkani <rashwan87@gmail.com>
2016-08-27 15:33:39 +0200

    Uppdaterat till Västtrafiks nya API

--- Constants.swift
+++ Constants.swift
@@ -9,8 +9,9 @@
 import Foundation
 
 struct Constants{
-    static let VTurl = "https://api.vasttrafik.se/bin/rest.exe/v1/"
+    static let VTurl = "https://api.vasttrafik.se/bin/rest.exe/v2/"
     static let VTauth = "1172d818-c330-435c-897c-9830750341c0"
+    static let VTkeysecret = "jZLPrtMTaS2aP2lUolla3ORfQfwa:5Qcty0F1HD_FfeExo3kDmHqWkqoa"
     static let VTerrorCode = "R0007"
     static let AppId = "1073431878"
 }
--- RestApiService.swift
+++ RestApiService.swift
@@ -20,42 +20,26 @@ extension Int  {
 class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
     static let sharedInstance = RestApiService()
     
+    // v2
     func getNearestStops(lat: String, long: String, onCompletion: (JSON) -> Void){
-        let url = "\(Constants.VTurl)location.nearbystops?authKey=\(Constants.VTauth)&format=json&originCoordLat=\(lat)&originCoordLong=\(long)&maxNo=50&MaxDist=3000"
+        let url = "\(Constants.VTurl)location.nearbystops?originCoordLat=\(lat)&originCoordLong=\(long)&maxNo=50&MaxDist=3000&format=json"
         
-        makeHTTPGetRequest(url, onCompletion: { json, err in
+        getToken(url, onCompletion: {json in
             onCompletion(json as JSON)
         })
     }
     
+    // v2
     func findStops(userInput: String, onCompletion: (JSON) -> Void){
         let escapedUserInput = userInput.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
-        let url = "\(Constants.VTurl)location.name?authKey=\(Constants.VTauth)&format=json&input=\(escapedUserInput)"
+        let url = "\(Constants.VTurl)location.name?input=\(escapedUserInput)&format=json"
         
-        makeHTTPGetRequest(url, onCompletion: { json, err in
-            onCompletion(json as JSON)
+        getToken(url, onCompletion: {json in
+            return json
         })
     }
     
-    func findLinesOnStopAtTime (stopId: String, onCompletion: (JSON) -> Void){
-        let date = NSDate()
-        let formatterTime = NSDateFormatter()
-        formatterTime.timeStyle = .ShortStyle
-        formatterTime.dateFormat = "HH:mm"
-        let timeString = formatterTime.stringFromDate(date)
-        
-        let formatterDate = NSDateFormatter()
-        formatterDate.dateStyle = .ShortStyle
-        formatterDate.dateFormat = "yyyy-MM-dd"
-        let dateString = formatterDate.stringFromDate(date)
-    
-        let url = "\(Constants.VTurl)departureBoard?authKey=\(Constants.VTauth)&format=json&id=\(stopId)&date=\(dateString)&time=\(timeString)"
-        
-        makeHTTPGetRequest(url, onCompletion: { json, err in
-            onCompletion(json as JSON)
-        })
-    }
-    
+    // v2
     func findAllLinesOnStop (stopId: String, onCompletion: (JSON) -> Void){
         var date = NSDate()
         let dateFormattera = NSDateFormatter()
@@ -70,16 +54,17 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
         formatterDate.dateFormat = "yyyy-MM-dd"
         
         let dateString = formatterDate.stringFromDate(date) //Convert to String
+        let timeString = formatterTime.stringFromDate(date)
+        var url = "\(Constants.VTurl)departureBoard?id=\(stopId)&date=\(dateString)&time=\(timeString)&timeSpan=60&maxDeparturesPerLine=1&format=json"
         
-        var url = "\(Constants.VTurl)departureBoard?authKey=\(Constants.VTauth)&format=json&id=\(stopId)&date=\(dateString)&timeSpan=60&maxDeparturesPerLine=1"
-        makeHTTPGetRequest(url, onCompletion: { json, err in
+        getToken(url, onCompletion: {json in
             let result = json["DepartureBoard"]["Departure"]
             if (result.count == 0){
                 var dateString = formatterDate.stringFromDate(date)
                 date = DateHelper.get(DateHelper.SearchDirection.Next, "Monday")
                 dateString = formatterDate.stringFromDate(date)
                 
-                url = "\(Constants.VTurl)departureBoard?authKey=\(Constants.VTauth)&format=json&id=\(stopId)&date=\(dateString)&timeSpan=60&maxDeparturesPerLine=1"
+                url = "\(Constants.VTurl)departureBoard?id=\(stopId)&date=\(dateString)&time=\(timeString)&timeSpan=60&maxDeparturesPerLine=1&format=json"
                 self.makeHTTPGetRequest(url, onCompletion: { json, err in
                     onCompletion(json as JSON)
                 })
@@ -89,9 +74,28 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
         })
     }
     
+    // v2
     func getDeparturesAtStop (stopId: String, onCompletion: (JSON) -> Void){
-        let url = "\(Constants.VTurl)departureBoard?authKey=\(Constants.VTauth)&format=json&id=\(stopId)&timeSpan=60&maxDeparturesPerLine=2"
-        makeHTTPGetRequest(url, onCompletion: { json, err in
+        let date = NSDate()
+        let dateFormattera = NSDateFormatter()
+        dateFormattera.dateFormat = "EEEE"
+        
+        let formatterTime = NSDateFormatter()
+        formatterTime.timeStyle = .ShortStyle //Set style of time
+        formatterTime.dateFormat = "HH:mm"
+        
+        let formatterDate = NSDateFormatter()
+        formatterDate.dateStyle = .ShortStyle //Set style of date
+        formatterDate.dateFormat = "yyyy-MM-dd"
+        
+        let dateString = formatterDate.stringFromDate(date) //Convert to String
+        let timeString = formatterTime.stringFromDate(date)
+        
+        let escapedString = timeString.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+        
+        let url = "\(Constants.VTurl)departureBoard?id=\(stopId)&date=\(dateString)&time=\(escapedString)&timeSpan=60&maxDeparturesPerLine=2&format=json"
+        
+        getToken(url, onCompletion: {json in
             onCompletion(json as JSON)
         })
     }
@@ -105,9 +109,41 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
         return futureDate!
     }
     
-    private func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
-        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+    private func getToken(url: String, onCompletion: (JSON) -> Void){
+        let data = Constants.VTkeysecret.dataUsingEncoding(NSUTF8StringEncoding)
+        let base64 = data!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
         
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.vasttrafik.se/token")!)
+        request.HTTPMethod = "POST"
+        let bodyData = "grant_type=client_credentials&scope=\(UIDevice.currentDevice().identifierForVendor!.UUIDString)"
+        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding)
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic \(base64)", forHTTPHeaderField: "Authorization")
+        
+        let urlconfig = NSURLSessionConfiguration.defaultSessionConfiguration()
+        urlconfig.timeoutIntervalForRequest = 15
+        urlconfig.timeoutIntervalForResource = 15
+        let session = NSURLSession(configuration: urlconfig, delegate: self, delegateQueue: nil)
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            if data == nil{
+                print(error!)
+                return
+            }
+            let json:JSON = JSON(data: data!)
+            NSUserDefaults(suiteName: "group.tajma.today")!.setObject(json["access_token"].string!, forKey: "token")
+            print("Token " + NSUserDefaults(suiteName: "group.tajma.today")!.stringForKey("token")!)
+            
+            self.makeHTTPGetRequest(url, onCompletion: { json, err in
+                onCompletion(json as JSON)
+            })
+        })
+        task.resume()
+    }
+    
+    private func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
+        print("Bearer \(NSUserDefaults(suiteName: "group.tajma.today")!.stringForKey("token")!)")
+        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
+        request.addValue("Bearer \(NSUserDefaults(suiteName: "group.tajma.today")!.stringForKey("token")!)", forHTTPHeaderField: "Authorization")
         let urlconfig = NSURLSessionConfiguration.defaultSessionConfiguration()
         urlconfig.timeoutIntervalForRequest = 15
         urlconfig.timeoutIntervalForResource = 15
@@ -115,11 +151,11 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
         
         let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
             if data == nil{
+                print(error)
                 onCompletion(nil, error)
                 return
             }
-            let json:JSON = JSON(data: data!)
-            onCompletion(json, error)
+            onCompletion(JSON(data: data!), error)
         })
         task.resume()
     }
