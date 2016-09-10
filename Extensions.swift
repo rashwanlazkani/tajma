fab755dfc26fc806f7ce2174999b3ccbc5536e14
Rashwan Lazkani <rashwan87@gmail.com>
2016-09-10 11:26:49 +0200

    Flyttat alla extensions till en och samma klass

--- Extensions.swift
+++ Extensions.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2016 Rashwan Lazkani. All rights reserved.
 //
 
+import ImageIO
+import UIKit
+
 extension Array {
     func firstOrDefault(fn: (Element) -> Bool) -> Element? {
         var to = self.filter(fn)
@@ -20,5 +23,201 @@ extension Array {
 extension Array where Element: Equatable {
     var orderedSetValue: Array  {
         return reduce([]){ $0.contains($1) ? $0 : $0 + [$1] }
+    }
+}
+
+extension UIImage {
+    
+    public class func gifWithData(data: NSData) -> UIImage? {
+        guard let source = CGImageSourceCreateWithData(data, nil) else {
+            print("SwiftGif: Source for the image does not exist")
+            return nil
+        }
+        return UIImage.animatedImageWithSource(source)
+    }
+    
+    public class func gifWithName(name: String) -> UIImage? {
+        guard let bundleURL = NSBundle.mainBundle().URLForResource(name, withExtension: "gif") else {
+            print("SwiftGif: This image named \"\(name)\" does not exist")
+            return nil
+        }
+        guard let imageData = NSData(contentsOfURL: bundleURL) else {
+            print("SwiftGif: Cannot turn image named \"\(name)\" into NSData")
+            return nil
+        }
+        return gifWithData(imageData)
+    }
+    
+    class func delayForImageAtIndex(index: Int, source: CGImageSource!) -> Double {
+        var delay = 0.1
+        
+        // Get dictionaries
+        let cfProperties = CGImageSourceCopyPropertiesAtIndex(source, index, nil)
+        let gifProperties: CFDictionaryRef = unsafeBitCast(
+            CFDictionaryGetValue(cfProperties,
+                unsafeAddressOf(kCGImagePropertyGIFDictionary)),
+            CFDictionary.self)
+        
+        // Get delay time
+        var delayObject: AnyObject = unsafeBitCast(
+            CFDictionaryGetValue(gifProperties,
+                unsafeAddressOf(kCGImagePropertyGIFUnclampedDelayTime)),
+            AnyObject.self)
+        if delayObject.doubleValue == 0 {
+            delayObject = unsafeBitCast(CFDictionaryGetValue(gifProperties,
+                unsafeAddressOf(kCGImagePropertyGIFDelayTime)), AnyObject.self)
+        }
+        
+        delay = delayObject as! Double
+        
+        if delay < 0.1 {
+            delay = 0.1 // Make sure they're not too fast
+        }
+        
+        return delay
+    }
+    
+    class func gcdForPair(a: Int?, b: Int?) -> Int {
+        var a = a
+        var b = b
+        // Check if one of them is nil
+        if b == nil || a == nil {
+            if b != nil {
+                return b!
+            } else if a != nil {
+                return a!
+            } else {
+                return 0
+            }
+        }
+        
+        // Swap for modulo
+        if a < b {
+            let c = a
+            a = b
+            b = c
+        }
+        
+        // Get greatest common divisor
+        var rest: Int
+        while true {
+            rest = a! % b!
+            
+            if rest == 0 {
+                return b! // Found it
+            } else {
+                a = b
+                b = rest
+            }
+        }
+    }
+    
+    class func gcdForArray(array: Array<Int>) -> Int {
+        if array.isEmpty {
+            return 1
+        }
+        
+        var gcd = array[0]
+        
+        for val in array {
+            gcd = UIImage.gcdForPair(val, b: gcd)
+        }
+        
+        return gcd
+    }
+    
+    class func animatedImageWithSource(source: CGImageSource) -> UIImage? {
+        let count = CGImageSourceGetCount(source)
+        var images = [CGImageRef]()
+        var delays = [Int]()
+        
+        // Fill arrays
+        for i in 0..<count {
+            // Add image
+            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(image)
+            }
+            
+            // At it's delay in cs
+            let delaySeconds = UIImage.delayForImageAtIndex(Int(i),
+                                                            source: source)
+            delays.append(Int(delaySeconds * 1000.0)) // Seconds to ms
+        }
+        
+        // Calculate full duration
+        let duration: Int = {
+            var sum = 0
+            
+            for val: Int in delays {
+                sum += val
+            }
+            
+            return sum
+        }()
+        
+        // Get frames
+        let gcd = gcdForArray(delays)
+        var frames = [UIImage]()
+        
+        var frame: UIImage
+        var frameCount: Int
+        for i in 0..<count {
+            frame = UIImage(CGImage: images[Int(i)])
+            frameCount = Int(delays[Int(i)] / gcd)
+            
+            for _ in 0..<frameCount {
+                frames.append(frame)
+            }
+        }
+        
+        // Heyhey
+        let animation = UIImage.animatedImageWithImages(frames,
+                                                        duration: Double(duration) / 1000.0)
+        
+        return animation
+    }
+    
+}
+
+extension UIColor {
+    convenience init(rgba: String) {
+        var red:   CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue:  CGFloat = 0.0
+        var alpha: CGFloat = 1.0
+        
+        if rgba.hasPrefix("#") {
+            let index   = rgba.startIndex.advancedBy(1)
+            let hex     = rgba.substringFromIndex(index)
+            let scanner = NSScanner(string: hex)
+            var hexValue: CUnsignedLongLong = 0
+            if scanner.scanHexLongLong(&hexValue) {
+                switch (hex.characters.count) {
+                case 3:
+                    red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
+                    green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
+                    blue  = CGFloat(hexValue & 0x00F)              / 15.0
+                case 4:
+                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
+                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
+                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
+                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
+                case 6:
+                    red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
+                    green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
+                    blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
+                case 8:
+                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
+                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
+                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
+                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
+                default:
+                    break
+                }
+            } else {
+            }
+        } else {
+        }
+        self.init(red:red, green:green, blue:blue, alpha:alpha)
     }
 }
