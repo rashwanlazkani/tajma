f8fd788e64c3148ec71589c6540afe64ea29bd78
Rashwan Lazkani <rashwan87@gmail.com>
2016-09-14 21:42:05 +0200

    Uppdaterat till Alamofire anrop

--- RestApiService.swift
+++ RestApiService.swift
@@ -6,20 +6,15 @@
 //  Copyright (c) 2015 Rashwan Lazkani. All rights reserved.
 //
 
+import Alamofire
 import Foundation
 import UIKit
 
 typealias ServiceResponse = (JSON, NSError?) -> Void
 
-extension Int  {
-    var day: (Int, NSCalendarUnit) {
-        return (self, NSCalendarUnit.Calendar.union(.Day))
-    }
-}
-
 class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
     static let sharedInstance = RestApiService()
-    
+
     func getNearestStops(lat: String, long: String, onCompletion: (JSON) -> Void){
         let url = "\(Constants.restURL)location.nearbystops?originCoordLat=\(lat)&originCoordLong=\(long)&maxNo=50&MaxDist=3000&format=json"
         
@@ -38,8 +33,6 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
     }
     
     func findAllLinesOnStop (stopId: String, onCompletion: (JSON) -> Void){
-        
-        
         var date = NSDate()
         let dateFormattera = NSDateFormatter()
         dateFormattera.dateFormat = "EEEE"
@@ -64,7 +57,8 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
                 dateString = formatterDate.stringFromDate(date)
                 
                 url = "\(Constants.restURL)departureBoard?id=\(stopId)&date=\(dateString)&time=\(timeString)&timeSpan=60&maxDeparturesPerLine=1&format=json"
-                self.makeHTTPGetRequest(url, onCompletion: { json, err in
+                
+                self.getToken(url, onCompletion: {json in
                     onCompletion(json as JSON)
                 })
                 return
@@ -94,8 +88,11 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
         let url = "\(Constants.restURL)departureBoard?id=\(stopId)&date=\(dateString)&time=\(escapedString)&timeSpan=60&maxDeparturesPerLine=2&format=json"
         
         getToken(url, onCompletion: {json in
+            print(json)
             onCompletion(json as JSON)
         })
+        
+        
     }
     
     private func addDays(date: NSDate, additionalDays: Int) -> NSDate {
@@ -111,49 +108,44 @@ class RestApiService: NSObject, NSURLSessionDelegate, NSURLSessionDataDelegate {
         let data = ("\(Constants.key):\(Constants.secret)").dataUsingEncoding(NSUTF8StringEncoding)
         let base64 = data!.base64EncodedStringWithOptions(NSDataBase64EncodingOptions(rawValue: 0))
         
-        let request = NSMutableURLRequest(URL: NSURL(string: Constants.tokenURL)!)
-        request.HTTPMethod = "POST"
-        let bodyData = "grant_type=client_credentials&scope=\(UIDevice.currentDevice().identifierForVendor!.UUIDString)"
-        request.HTTPBody = bodyData.dataUsingEncoding(NSUTF8StringEncoding)
-        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-        request.addValue("Basic \(base64)", forHTTPHeaderField: "Authorization")
+        let parameters = [
+            "grant_type": "client_credentials",
+            "scope": "\(UIDevice.currentDevice().identifierForVendor!.UUIDString)"
+        ]
         
-        let urlconfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-        urlconfig.timeoutIntervalForRequest = 15
-        urlconfig.timeoutIntervalForResource = 15
-        let session = NSURLSession(configuration: urlconfig, delegate: self, delegateQueue: nil)
-        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            if data == nil{
-                print(error!)
-                return
-            }
-            let tokenJson:JSON = JSON(data: data!)
-            NSUserDefaults(suiteName: "group.tajma.today")!.setObject(tokenJson["access_token"].string!, forKey: "token")
-            
-            self.makeHTTPGetRequest(url, onCompletion: { json, err in
-                onCompletion(json as JSON)
-            })
-        })
-        task.resume()
-    }
-    
-    private func makeHTTPGetRequest(path: String, onCompletion: ServiceResponse) {
-        //print("Bearer \(NSUserDefaults(suiteName: "group.tajma.today")!.stringForKey("token")!)")
-        let request = NSMutableURLRequest(URL: NSURL(string: path)!)
-        request.addValue("Bearer \(NSUserDefaults(suiteName: "group.tajma.today")!.stringForKey("token")!)", forHTTPHeaderField: "Authorization")
-        let urlconfig = NSURLSessionConfiguration.defaultSessionConfiguration()
-        urlconfig.timeoutIntervalForRequest = 15
-        urlconfig.timeoutIntervalForResource = 15
-        let session = NSURLSession(configuration: urlconfig, delegate: self, delegateQueue: nil)
-        
-        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
-            if data == nil{
-                print(error)
-                onCompletion(nil, error)
-                return
-            }
-            onCompletion(JSON(data: data!), error)
-        })
-        task.resume()
+        let headers = [
+            "Authorization": "Basic \(base64)",
+            "Content-Type": "application/x-www-form-urlencoded"
+        ]
+
+        Alamofire.request(.POST, Constants.tokenURL, parameters: parameters, headers: headers, encoding: .URLEncodedInURL)
+            .responseJSON { response in
+                if let json = response.result.value {
+                    if let token = json["access_token"]!{
+                        print(token)
+                        
+                        let headers = [
+                            "Authorization": "Bearer \(token)",
+                            "Content-Type": "application/x-www-form-urlencoded"
+                        ]
+                        
+                        Alamofire.request(.GET, url, headers: headers)
+                            .responseJSON { response in
+                                switch response.result {
+                                case .Success:
+                                    let json = JSON(data: response.data!)
+                                    print(json)
+                                    onCompletion(json)
+                                case .Failure(let error):
+                                    print(error)
+                                    //onCompletion(error)
+                                }
+                        }
+                    }
+                }
+                else{
+                    print(response.description)
+                }
+        }
     }
 }
