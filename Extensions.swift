0135291749f6969b5e5e7d9b1e1b4113d25c1fed
Rashwan Lazkani <rashwan87@gmail.com>
2017-03-29 23:20:14 +0200

    Fixat ny extension för konventering av hex > RGB

--- Extensions.swift
+++ Extensions.swift
@@ -60,6 +60,27 @@ extension Int  {
     }
 }
 
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex.replacingOccurrences(of: "#", with: ""))
+        scanner.scanLocation = 0
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+}
+
 extension Date {
     struct Formatter {
         static let custom: DateFormatter = {
