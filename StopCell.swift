e778e69dd0523d6b5d9c6a5bc321cb025cf933d4
Rashwan Lazkani <rashwan87@gmail.com>
2017-04-07 15:55:29 +0200

    Lagt till nya resurser i projektet

---
+++ StopCell.swift
@@ -0,0 +1,24 @@
+//
+//  StopCell.swift
+//  Tajma
+//
+//  Created by Rashwan Lazkani on 2017-04-01.
+//  Copyright © 2017 Rashwan Lazkani. All rights reserved.
+//
+
+import UIKit
+
+class StopCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
